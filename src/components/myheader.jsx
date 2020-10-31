afedef9e08e2269597288fbca360597f94543b20
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-31 13:11:28 +0200

    Color change

--- src/components/myheader.jsx
+++ src/components/myheader.jsx
@@ -1,7 +1,7 @@
 export const MyHeader = () => {
     return (
         <div className="container-sm col-md-6 col-md-offset-3 p-2 align-items-center text-center">
-            <h2 className="text-primary">Δοκιμαστικός Header</h2>
+            <h2 className="text-danger">Δοκιμαστικός Header</h2>
         </div>
     );
 };
