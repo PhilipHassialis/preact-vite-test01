4ec91594468a3bc1b49c37de37e0fe957ff5522f
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2020-10-31 12:01:57 +0200

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,64 @@
+import { useState, useMemo } from "preact/hooks";
+
+export const App = (props) => {
+    let [counter, setCounter] = useState(0);
+    let [textForReversal, setTextForReversal] = useState("");
+
+    const reversedText = textForReversal.split("").reverse().join("");
+
+    return (
+        <>
+            <div class="container-sm col-md-6 col-md-offset-3 p-2">
+                <form>
+                    <div class="mb-3">
+                        <label for="straightForwardName" class="form-label">
+                            Εισάγετε όνομα:
+                        </label>
+                        <input
+                            id="straightForwardName"
+                            class="form-control"
+                            placeholder="γράψτε κάτι..."
+                            onChange={(e) => {
+                                setTextForReversal(e.target.value);
+                            }}
+                            onKeyDown={(e) => {
+                                setTextForReversal(e.target.value);
+                            }}
+                            onKeyUp={(e) => {
+                                setTextForReversal(e.target.value);
+                            }}
+                        ></input>
+                    </div>
+                    <div class="mb-3">
+                        <label for="reversedName" class="form-label">
+                            Ανστροφή:
+                        </label>
+                        <input
+                            id="reversedName"
+                            class="form-control"
+                            value={reversedText}
+                            readOnly="readOnly"
+                        ></input>
+                    </div>
+                    <div class="mb-3">
+                        <label for="counterValue" class="form-label">
+                            Μετρητής:
+                        </label>
+                        <input
+                            class="form-control mb-2"
+                            id="counterValue"
+                            value={counter}
+                            readOnly="readOnly"
+                        ></input>
+                        <div
+                            class="btn btn-primary"
+                            onClick={(e) => setCounter(++counter)}
+                        >
+                            Επαύξησις
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+};
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { render } from "preact";
+import { App } from "./app";
+import "./css/bootstrap.css";
+
+render(<App />, document.getElementById("app"));
